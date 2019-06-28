df46417866942d157d2cf40cc07a6e8022a3af1a
Jason King <jason.king@joyent.com>
2019-06-27 21:37:20 -0500

    Silence attribute warnings in chapoly headers

--- chapoly/chacha.h
+++ chapoly/chacha.h
@@ -11,6 +11,7 @@ Public domain.
 
 #include <sys/types.h>
 #include <stdlib.h>
+#include "../utils.h"
 
 struct chacha_ctx {
 	u_int input[16];
@@ -23,14 +24,14 @@ struct chacha_ctx {
 #define CHACHA_BLOCKLEN		64
 
 void chacha_keysetup(struct chacha_ctx *x, const u_char *k, u_int kbits)
-    __attribute__((__bounded__(__minbytes__, 2, CHACHA_MINKEYLEN)));
+    __bounded(__minbytes__, 2, CHACHA_MINKEYLEN);
 void chacha_ivsetup(struct chacha_ctx *x, const u_char *iv, const u_char *ctr)
-    __attribute__((__bounded__(__minbytes__, 2, CHACHA_NONCELEN)))
-    __attribute__((__bounded__(__minbytes__, 3, CHACHA_CTRLEN)));
+    __bounded(__minbytes__, 2, CHACHA_NONCELEN)
+    __bounded(__minbytes__, 3, CHACHA_CTRLEN);
 void chacha_encrypt_bytes(struct chacha_ctx *x, const u_char *m,
     u_char *c, u_int bytes)
-    __attribute__((__bounded__(__buffer__, 2, 4)))
-    __attribute__((__bounded__(__buffer__, 3, 4)));
+    __bounded(__buffer__, 2, 4)
+    __bounded(__buffer__, 3, 4);
 
 #endif	/* CHACHA_H */
 
