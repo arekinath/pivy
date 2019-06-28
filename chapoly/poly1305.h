2dfc576242863f5e96fd249d8336026b9e73667e
Jason King <jason.king@joyent.com>
2019-06-27 21:37:20 -0500

    Silence attribute warnings in chapoly headers

--- chapoly/poly1305.h
+++ chapoly/poly1305.h
@@ -9,14 +9,15 @@
 #define POLY1305_H
 
 #include <sys/types.h>
+#include "../utils.h"
 
 #define POLY1305_KEYLEN		32
 #define POLY1305_TAGLEN		16
 
 void poly1305_auth(u_char out[POLY1305_TAGLEN], const u_char *m, size_t inlen,
     const u_char key[POLY1305_KEYLEN])
-    __attribute__((__bounded__(__minbytes__, 1, POLY1305_TAGLEN)))
-    __attribute__((__bounded__(__buffer__, 2, 3)))
-    __attribute__((__bounded__(__minbytes__, 4, POLY1305_KEYLEN)));
+    __bounded(__minbytes__, 1, POLY1305_TAGLEN)
+    __bounded(__buffer__, 2, 3)
+    __bounded(__minbytes__, 4, POLY1305_KEYLEN);
 
 #endif	/* POLY1305_H */
